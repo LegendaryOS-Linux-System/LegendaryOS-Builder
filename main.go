6894406edca8626b0fa76729a16cb40843134d64
michal92299 <chudybyczek2137@gmail.com>
2026-05-12 07:58:50 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"github.com/legendaryos/builder/src/cli"
+)
+
+// Set by -ldflags at build time
+var (
+	Version   = "dev"
+	Commit    = "unknown"
+	BuildDate = "unknown"
+)
+
+func main() {
+	cli.Version = Version
+	cli.Commit = Commit
+	cli.BuildDate = BuildDate
+	cli.Execute()
+}
